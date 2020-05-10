dbad47cb2e48cae2bd58510c4d7d5e242442f0fa
sachinblogs <65084387+sachinblogs@users.noreply.github.com>
2020-05-10 18:32:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,6 @@ def instance_record_process(blob_refe,bq_refe,ds_nm,ds_tbl_nm,inv_dt):
     fl_records = blob_refe.download_as_string()
     records = fl_records.decode('utf8').split('\n')
     for rec in records:
-        print(rec)
         if rec != "":
             j_rec = json.loads(rec)
             nm_attr = j_rec['name'].split('/')
@@ -60,6 +59,89 @@ def instance_record_process(blob_refe,bq_refe,ds_nm,ds_tbl_nm,inv_dt):
                 print(rows_insert)
             else:
                 print(errors)
+    
+def bucket_record_process(blob_refe,bq_refe,ds_nm,ds_tbl_nm,project_nm,inv_dt):
+    dataset = bq_refe.dataset(ds_nm)
+    try:
+        table = bq_refe.get_table(ds_tbl_nm)
+    except NotFound:
+        print('Table {} not found'.format(ds_tbl_nm))
+        return False
+    fl_records = blob_refe.download_as_string()
+    records = fl_records.decode('utf8').split('\n')
+    for rec in records:
+        if rec != "":
+            j_rec = json.loads(rec)
+            prj_nm =  project_nm
+            buck_nm_attr = j_rec['name'].split('/')
+            buck_nm = buck_nm_attr[3]
+            buck_loc = j_rec['resource']['data']['location'].lower()
+            buck_loc_type = j_rec['resource']['data']['locationType']
+            buck_prj_num = j_rec['resource']['data']['projectNumber']
+            buck_stg_class = j_rec['resource']['data']['storageClass']
+            buck_cr_dt = j_rec['resource']['data']['timeCreated']
+            buck_iam_buckpolicy = False
+            if 'enabled' in j_rec['resource']['data']['iamConfiguration']['bucketPolicyOnly']:
+                buck_iam_buckpolicy = j_rec['resource']['data']['iamConfiguration']['bucketPolicyOnly']['enabled']
+            buck_rule_str = ''
+            i = 0
+            for rl in j_rec['resource']['data']['lifecycle']['rule']:
+                if i == 0:
+                    buck_rule_str = str(rl)
+                    i = i + 1
+                else:
+                    buck_rule_str = buck_rule_str+'|'+str(rl)
+            buck_iam_buckpolicy_lck_tm = time.time()
+            buck_iam_uniform_lck_tm = time.time()
+            if buck_iam_buckpolicy:
+                  buck_iam_buckpolicy_lck_tm = j_rec['resource']['data']['iamConfiguration']['bucketPolicyOnly']['lockedTime']
+            buck_iam_uniform = False
+            if 'enabled' in j_rec['resource']['data']['iamConfiguration']['uniformBucketLevelAccess']:
+                buck_iam_uniform = j_rec['resource']['data']['iamConfiguration']['uniformBucketLevelAccess']['enabled']
+            if buck_iam_uniform:
+                 buck_iam_uniform_lck_tm = j_rec['resource']['data']['iamConfiguration']['uniformBucketLevelAccess']['lockedTime']
+            try:
+                buck_retn_policy_period = j_rec['resource']['data']['retentionPolicy']['retentionPeriod']
+            except KeyError:
+                buck_retn_policy_period = -1
+            try:
+                buck_retn_policy_locked = j_rec['resource']['data']['retentionPolicy']['isLocked']
+            except KeyError:
+                buck_retn_policy_locked = "False"
+            try:
+                buck_retn_policy_eff_tm = j_rec['resource']['data']['retentionPolicy']['effectiveTime']
+            except KeyError:
+                buck_retn_policy_eff_tm = time.time()
+            buck_sz = 0
+            client = monitoring_v3.MetricServiceClient()
+            project_name = client.project_path(project_nm)
+            interval = monitoring_v3.types.TimeInterval()
+            now = time.time()
+            interval.end_time.seconds = int(now)
+            interval.end_time.nanos = int(
+                    (now - interval.end_time.seconds) * 10**9)
+            interval.start_time.seconds = int(now - 3600)
+            interval.start_time.nanos = interval.end_time.nanos
+            aggregation = monitoring_v3.types.Aggregation()
+            aggregation.alignment_period.seconds = 3600  # 20 minutes
+            aggregation.per_series_aligner = (
+                    monitoring_v3.enums.Aggregation.Aligner.ALIGN_MEAN)
+            filter = 'resource.type = "gcs_bucket" AND resource.labels.bucket_name = \"'+buck_nm+'\" AND resource.labels.location = '+buck_loc+' AND resource.labels.project_id = '+project_nm+' AND metric.type = "storage.googleapis.com/storage/total_bytes"'
+            results = client.list_time_series(
+                    project_name,
+                    filter,
+                    interval,
+                    monitoring_v3.enums.ListTimeSeriesRequest.TimeSeriesView.FULL,
+                    aggregation)
+            for result in results:
+                buck_sz = result.points[0].value.double_value/(1024*1024)
+            rows_insert = [(prj_nm,buck_nm,buck_loc,buck_loc_type,buck_prj_num,buck_stg_class,buck_cr_dt,buck_iam_buckpolicy,buck_iam_buckpolicy_lck_tm,buck_iam_uniform,buck_iam_uniform_lck_tm,buck_rule_str,buck_retn_policy_period,buck_retn_policy_locked,buck_retn_policy_eff_tm,buck_sz,inv_dt)]
+            errors = bq_refe.insert_rows(table,rows_insert)
+            if errors == []:
+                print("Bucket Record added")
+                print(rows_insert)
+            else:
+                print(errors)
                 
     def gcs_inventory_trigger(data, context):
         storage_client = storage.Client()
@@ -85,3 +167,9 @@ def instance_record_process(blob_refe,bq_refe,ds_nm,ds_tbl_nm,inv_dt):
                 blob = bucket.blob(blob_nm)
                 DS_TABLE = DS_NAME+'.Instances'
                 instance_record_process(blob,bq_client,DS_NAME,DS_TABLE,inv_dt)
+            elif bob_post_fix_nm == "storage.googleapis.com/Bucket/0":
+                print("Processing Bucket Inventory")
+                bucket = storage_client.get_bucket(bucket_nm)
+                blob = bucket.blob(blob_nm)
+                DS_TABLE = DS_NAME+'.Buckets'
+                bucket_record_process(blob,bq_client,DS_NAME,DS_TABLE,inv_project,inv_dt
